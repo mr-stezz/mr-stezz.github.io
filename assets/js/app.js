54dcc7b91f3f28a10ace90fe8a0a40e8f16b7583
mr-stezz <mstezy@gmail.com>
2015-10-28 07:00:05 +0000

    Prune app js and make contact form work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,73 @@
-// sticky header
-// ;( function ( document, window, index )
-// {
-  // 'use strict';
-// 
-  // var elSelector  = '.page-header',
-    // element   = document.querySelector( elSelector );
-// 
-  // if( !element ) return true;
-// 
-  // var elHeight      = 0,
-    // elTop           = 0,
-    // dHeight         = 0,
-    // wHeight         = 0,
-    // wScrollCurrent  = 0,
-    // wScrollBefore   = 0,
-    // wScrollDiff     = 0;
-// 
-  // window.addEventListener( 'scroll', function()
-  // {
-    // elHeight    = element.offsetHeight;
-    // dHeight     = document.body.offsetHeight;
-    // wHeight     = window.innerHeight;
-    // wScrollCurrent  = window.pageYOffset;
-    // wScrollDiff   = wScrollBefore - wScrollCurrent;
-    // elTop     = parseInt( window.getComputedStyle( element ).getPropertyValue( 'top' ) ) + wScrollDiff;
-// 
-    // if( wScrollCurrent <= 0 ) // scrolled to the very top; element sticks to the top
-      // element.style.top = '0px';
-// 
-    // else if( wScrollDiff > 0 ) // scrolled up; element slides in
-      // element.style.top = ( elTop > 0 ? 0 : elTop ) + 'px';
-// 
-    // else if( wScrollDiff < 0 ) // scrolled down
-    // {
-      // if( wScrollCurrent + wHeight >= dHeight - elHeight )  // scrolled to the very bottom; element slides in
-        // element.style.top = ( ( elTop = wScrollCurrent + wHeight - dHeight ) < 0 ? elTop : 0 ) + 'px';
-// 
-      // else // scrolled down; element slides out
-        // element.style.top = ( Math.abs( elTop ) > elHeight ? -elHeight : elTop ) + 'px';
-    // }
-// 
-    // wScrollBefore = wScrollCurrent;
+  // David Darnes code:
+  var contactform = document.getElementById('js-form');
+  var reciever = document.getElementById('js-reciever');
+  var email = 'ahoy' + '@' + 'stezycki' + '.' + 'com';
+  contactform.setAttribute('action', '//formspree.io/' + email);
+  reciever.textContent = email;
+
+  // function isParent(parent, child) {
+  // var node = child.parentNode;
+  //   while (node != null) {
+  //     if (node == parent) {
+  //       return true;
+  //     }
+  //     node = node.parentNode;
+  //   }
+  //   return false;
+  // }
+
+  // var checkbox = document.querySelector('input[name="toggle-checkbox"]');
+  // var container = document.querySelector('.container');
+  // document.addEventListener('click', function(e) {
+  //   if (checkbox.checked == true) {
+  //   if (!isParent(container, e.target) && checkbox.checked == true) {
+  //     checkbox.checked = false;
+  //   }
   // });
-// 
-// }( document, window, 0 ));
+
+  // As per: http://jdp.org.uk/ajax/2015/05/20/ajax-forms-without-jquery.html
+  if (document.forms[0] && window.FormData) {
+
+    var message = new Object();
+    message.loading = 'Loading&hellip';
+    message.success = 'Please allow 48 hours for a response :)';
+    message.failure = 'Whoops! Something went wrong, please try again :(';
+
+    var statusMessage = document.createElement('div');
+    statusMessage.className = 'status';
+
+    // Set up the AJAX request
+    var request = new XMLHttpRequest();
+    request.open('POST', '//formspree.io/' + email, true);
+    request.setRequestHeader('accept', 'application/json');
+
+    // Listen for the form being submitted
+    contactform.addEventListener('submit', function(evt) {
+      evt.preventDefault();
+      contactform.appendChild(statusMessage);
+
+      // Create a new FormData object passing in the form's key value pairs (that was easy!)
+      var formData = new FormData(contactform);
+
+      // Send the formData
+      request.send(formData);
+
+      // Watch for changes to request.readyState and update the statusMessage accordingly
+      request.onreadystatechange = function() {
+        // <4 =  waiting on response from server
+        if (request.readyState < 4)
+          statusMessage.innerHTML = message.loading;
+        // 4 = Response from server has been completely loaded.
+        else if (request.readyState === 4) {
+          // 200 - 299 = successful
+          if (request.status == 200 && request.status < 300)
+            statusMessage.innerHTML = message.success;
+          else
+            contactform.insertAdjacentHTML('beforeend', message.failure);
+        }
+      }
+    });
+  }
 
 // molten leading
 moltenLeading("h1", {
@@ -64,9 +88,3 @@ moltenLeading("p", {
   minwidth: 400,
   maxwidth: 768
 });
-
-// masthead photo slider
-new IdealImageSlider.Slider('#slider');
-
-// initialise smooth scroll
-smoothScroll.init();
